44d5050a9a726057fa0b16d11c0551d5c67a3f12
Ana Quintao <ana.quintao@localiza.com>
2024-08-16 15:57:34 -0300

    Ajuste do código

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 let tituloH1 = document.getElementById("titulo")
 let ulLista = document.getElementById("lista-ordenada")
-let aLink = document.querySelector(a)
-let liLista = document.querySelector(li)
+let aLink = document.querySelector("a")
+let liLista = document.querySelector("li")
 
+tituloH1.innerText = "Titulo da Página"
 console.log(tituloH1.innerText);
-console.log(alink.innerText);
-console.log(ulLista.innerHTML) = "Item 1"; "item 2"; "Item 3";
-console.log(liLista.innerHTML) = "Item 1"; "item 2"; "Item 3";
-//.innerHTML para adicionar três itens simples na lista não ordenada, e três itens com links para outros sites na lista ordenada.  
+aLink.innerText = "Link da Proz"
+console.log(alink.innerText); 
+//console.log(ulLista.innerHTML) = "Item 1"; "item 2"; "Item 3";
+//console.log(liLista.innerHTML) = "Item 1"; "item 2"; "Item 3";
