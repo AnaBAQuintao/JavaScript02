8e4623fec63254bcf958c795751cd7dd510945d6
Ana Quintao <ana.quintao@localiza.com>
2024-08-16 14:49:14 -0300

    Exercicio 02

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let tituloH1 = document.getElementById("titulo")
+let ulLista = document.getElementById("lista-ordenada")
+let aLink = document.querySelector(a)
+let liLista = document.querySelector(li)
+
+console.log(tituloH1.innerText);
+console.log(alink.innerText);
+console.log(ulLista.innerHTML) = "Item 1"; "item 2"; "Item 3";
+console.log(liLista.innerHTML) = "Item 1"; "item 2"; "Item 3";
+//.innerHTML para adicionar três itens simples na lista não ordenada, e três itens com links para outros sites na lista ordenada.  
